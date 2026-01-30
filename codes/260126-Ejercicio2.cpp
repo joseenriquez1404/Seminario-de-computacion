c50b4fdc72b3d7cb10843d0625948fc09e6cc93a
José Enríquez <manuel.enriquezr@uanl.edu.mx>
2026-01-29 18:00:35 -0600

    Primeros 3 problemas

---
+++ codes/260126-Ejercicio2.cpp
@@ -0,0 +1,60 @@
+/*
+ * Nombre: Jose Manuel Enriquez Rodriguez
+ * Matricula: 2132982
+* Problema:Leer n datos enteros, primero leer el valor de n. Almacenarlos en un vector, después pasar a otro vector, primero todos los pares y después todos lo impasres
+Después mostrar, primero todos los impares, en una misma línea, luego en otra línea todos lo pares, también calcular el promedio de todos lo pares, mostrar en nueva línea este ultimo resultado
+ *
+ */
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+ int n;
+ cin >> n;
+ vector<int> vectorBase;
+ vector<int> finalVector;
+
+ // Almacenar los n elementos en el primer vector
+ for (int i = 0; i < n; i++) {
+  int number;
+  cin >> number;
+  vectorBase.push_back(number);
+ }
+
+ vector<int> pares;
+ vector<int> impares;
+
+ // Separamos a los pares de los impares en dos vectores diferentes
+ for (int i = 0; i < vectorBase.size(); i++) {
+  if (vectorBase[i] % 2 == 0) {
+   pares.push_back(vectorBase[i]);
+  } else {
+   impares.push_back(vectorBase[i]);
+  }
+ }
+
+ for (int x: pares) finalVector.push_back(x);
+ for (int x: impares) finalVector.push_back(x);
+
+ for (int x: impares) {
+  cout << x << " ";
+ }
+ cout << endl;
+
+ for (int x: pares) {
+  cout << x << " ";
+ }
+ cout << endl;
+
+ if (!pares.empty()) {
+  int suma = 0;
+  for (int x : pares) suma += x;
+  cout << "El promedio de los numeros pares es: " << suma / pares.size() << endl;
+ } else {
+  cout << "El promedio de los numeros pares es 0" << endl;
+ }
+ return 0;
+}
